7f75408f9e154c4b11d66d69fecafcda68514614
Janet <dogandkoalabear@gmail.com>
2015-10-14 02:00:47 -0700

    first js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(".tag").hide().show("slow");
+
+// Hide result
+$(".result img").hide();
+// Add a constant graph
+var $constant = $(".result").html('<img src="img/constant.png" style="width: 40%; height: 40%">');
+// when constant graph pressed 
+
+// var location = $constant.attr("href");
+
+//console.log($constant.attr("src", location));
+
+// $overlay.click(function(event) {
+// 	event.preventDefault();
+//  	$overlay.hide();
+//   	$overlay.prev().show();
+//  });
