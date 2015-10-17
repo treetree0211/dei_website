b33c59f0ec2679290368f964187230c5e3ba9a05
Janet <dogandkoalabear@gmail.com>
2015-10-17 15:00:41 -0700

    second edition js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,4 @@
-$(".tag").hide().show("slow");
-
-// Hide result
-$(".result img").hide();
-// Add a constant graph
-var $constant = $(".result").html('<img src="img/constant.png" style="width: 40%; height: 40%">');
-// when constant graph pressed 
-
-// var location = $constant.attr("href");
-
-//console.log($constant.attr("src", location));
-
-// $overlay.click(function(event) {
-// 	event.preventDefault();
-//  	$overlay.hide();
-//   	$overlay.prev().show();
-//  });
+function download(file)
+{
+ window.location=file;
+}
